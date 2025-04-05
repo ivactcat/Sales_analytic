2d2ec7c0fe17d7777f98df7bb867c6a0edf04d94
ivan <ivancatcam@gmail.com>
2025-04-05 19:11:56 +0100

    analytic

--- src/clean.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import numpy as np
-
-def dollars_float(dataframe,col):
-    # Replace the dollar symbol "$" in the specified column
-    dataframe[col]= dataframe[col].str.replace("$","").astype(float)
-    return dataframe # Return the modified DataFrame
